48f9b51925d20d6493d6816204c473f327dc79ff
ArchLeaders <archleadership28@gmail.com>
2026-07-28 11:36:23 -0700

    fix: Automatically collect asset hint at runtime

--- src/AalSharp.Bars/BarsEntry.cs
+++ src/AalSharp.Bars/BarsEntry.cs
@@ -1,10 +1,14 @@
+using System.Runtime.CompilerServices;
+using AalSharp.Helpers;
+
 namespace AalSharp.Bars;
 
 public sealed class BarsEntry
 {
-    public string? Hint { get; set; }
-    
+    public string? Hint
+        => Asset is [_, _, _, _, ..] ? PrimitivesHelper.ToAscii(Unsafe.As<byte, uint>(ref Asset[0])) : null;
+
     public required BarsMetadata Metadata { get; set; }
-    
+
     public required byte[] Asset { get; set; }
 }
