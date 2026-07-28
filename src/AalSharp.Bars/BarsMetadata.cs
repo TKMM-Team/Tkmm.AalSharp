8f03ff73624b4115050ea94f781ab1b5d64a27b9
ArchLeaders <archleadership28@gmail.com>
2026-07-28 11:35:50 -0700

    chore: Remove version field from BarsMetadata (v4 supported)

--- src/AalSharp.Bars/BarsMetadata.cs
+++ src/AalSharp.Bars/BarsMetadata.cs
@@ -2,8 +2,6 @@ namespace AalSharp.Bars;
 
 public sealed class BarsMetadata
 {
-    public ushort Version { get; set; }
-
     public required BarsMetadataData Data { get; set; }
     
     public required BarsMetadataMarker Marker { get; set; }
