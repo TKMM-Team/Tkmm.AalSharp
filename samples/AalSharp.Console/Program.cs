dbb53467b22b2a8c48bd7189109e4b6ceb420fcf
ArchLeaders <archleadership28@gmail.com>
2026-07-28 23:35:51 -0700

    chore: Update sample/test app

--- samples/AalSharp.Console/Program.cs
+++ samples/AalSharp.Console/Program.cs
@@ -1,16 +1,32 @@
 ﻿using AalSharp.Bars;
 using AalSharp.Bars.IO;
 
-var data = File.ReadAllBytes(args[0]);
-var bars = BarsFile.FromBinary(data, out var endianness);
+var outputFile = args[0] + ".out.bars";
 
-var output = args[0] + ".dir";
-Directory.CreateDirectory(output);
+{
+    var data = File.ReadAllBytes(args[0]);
+    var bars = BarsFile.FromBinary(data, out var endianness);
 
-foreach (var (hash, entry) in bars) {
-    File.WriteAllBytes(Path.Combine(output, $"0x{hash:x8}.amta"), AmtaSerializer.Serialize(entry.Metadata, endianness));
-    File.WriteAllBytes(Path.Combine(output, $"0x{hash:x8}.b{entry.Hint?.ToLower() ?? "in"}"), entry.Asset);
+    var output = args[0] + ".dir";
+    Directory.CreateDirectory(output);
+
+    foreach (var (hash, entry) in bars) {
+        File.WriteAllBytes(Path.Combine(output, $"{entry.Metadata.Data.Name}.amta"), AmtaSerializer.Serialize(entry.Metadata, endianness));
+        File.WriteAllBytes(Path.Combine(output, $"{entry.Metadata.Data.Name}.b{entry.Hint?.ToLower() ?? "in"}"), entry.Asset);
+    }
+
+    File.WriteAllBytes(outputFile, bars.ToBinary(endianness));
 }
 
-var outputFile = args[0] + ".out.bars";
-File.WriteAllBytes(outputFile, bars.ToBinary(endianness));
+{
+    var data = File.ReadAllBytes(outputFile);
+    var bars = BarsFile.FromBinary(data, out var endianness);
+
+    var output = outputFile + ".dir";
+    Directory.CreateDirectory(output);
+
+    foreach (var (hash, entry) in bars) {
+        File.WriteAllBytes(Path.Combine(output, $"{entry.Metadata.Data.Name}.amta"), AmtaSerializer.Serialize(entry.Metadata, endianness));
+        File.WriteAllBytes(Path.Combine(output, $"{entry.Metadata.Data.Name}.b{entry.Hint?.ToLower() ?? "in"}"), entry.Asset);
+    }
+}
