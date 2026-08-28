12495c55c4353a0c089c76dd762266d85e58a96b
ArchLeaders <archleadership28@gmail.com>
2026-08-27 22:12:42 -0700

    chore: Include duplicate files in test control

--- tests/Tkmm.AalSharp.Bars.Tests/SerializationTests.cs
+++ tests/Tkmm.AalSharp.Bars.Tests/SerializationTests.cs
@@ -9,16 +9,37 @@ public class SerializationTests
     private static readonly byte[] _asset2 = File.ReadAllBytes(Path.Combine(AppContext.BaseDirectory, "Files", "Control_2.bwav"));
     private static readonly byte[] _metadata1 = File.ReadAllBytes(Path.Combine(AppContext.BaseDirectory, "Files", "Control_1.amta"));
     private static readonly byte[] _metadata2 = File.ReadAllBytes(Path.Combine(AppContext.BaseDirectory, "Files", "Control_2.amta"));
+    private static readonly byte[] _metadata3 = File.ReadAllBytes(Path.Combine(AppContext.BaseDirectory, "Files", "Control_3.amta"));
 
-    private static readonly byte[] _file = File.ReadAllBytes(Path.Combine(AppContext.BaseDirectory, "Files", "Control.bars"));
+    private static readonly byte[] _file1 = File.ReadAllBytes(Path.Combine(AppContext.BaseDirectory, "Files", "Control_1.bars"));
+    private static readonly byte[] _file2 = File.ReadAllBytes(Path.Combine(AppContext.BaseDirectory, "Files", "Control_2.bars"));
 
-    private readonly AudioResource _control = new() {
-        ["Test"] = new AudioResourceAsset {
+    private readonly AudioResource _control1 = new() {
+        ["Test 1"] = new AudioResourceAsset {
             Metadata = _metadata1,
             Asset = _asset1
         },
         ["Test 2"] = new AudioResourceAsset {
             Metadata = _metadata2,
+            Asset = _asset2
+        },
+        ["Test 3"] = new AudioResourceAsset {
+            Metadata = _metadata3,
+            Asset = _asset1
+        }
+    };
+
+    private readonly AudioResource _control2 = new() {
+        ["Test 1"] = new AudioResourceAsset {
+            Metadata = _metadata1,
+            Asset = _asset1
+        },
+        ["Test 2"] = new AudioResourceAsset {
+            Metadata = _metadata2,
+            Asset = _asset1
+        },
+        ["Test 3"] = new AudioResourceAsset {
+            Metadata = _metadata3,
             Asset = _asset2
         }
     };
@@ -26,12 +47,14 @@ public class SerializationTests
     [Fact]
     public void Serialized_ShouldEqual_File()
     {
-        _control.ToBinary().Should().BeEquivalentTo(_file);
+        _control1.ToBinary().Should().BeEquivalentTo(_file1);
+        _control2.ToBinary().Should().BeEquivalentTo(_file2);
     }
     
     [Fact]
     public void Deserialized_ShouldEqual_Control()
     {
-        AudioResource.FromBinary(_file).Should().BeEquivalentTo(_control);
+        AudioResource.FromBinary(_file1).Should().BeEquivalentTo(_control1);
+        AudioResource.FromBinary(_file2).Should().BeEquivalentTo(_control2);
     }
 }
