87973a847962a28c0743668a214e9effe2a767d0
ArchLeaders <archleadership28@gmail.com>
2026-08-27 18:40:59 -0700

    fix: Faulty tests using old audio files
    - Use modern bwavs (no size information)
    - Use multiple audio files

--- tests/Tkmm.AalSharp.Bars.Tests/SerializationTests.cs
+++ tests/Tkmm.AalSharp.Bars.Tests/SerializationTests.cs
@@ -5,32 +5,33 @@ namespace Tkmm.AalSharp.Bars.Tests;
 
 public class SerializationTests
 {
-    private static readonly byte[] _asset = File.ReadAllBytes(Path.Combine(AppContext.BaseDirectory, "Files", "Control.bfwav"));
+    private static readonly byte[] _asset1 = File.ReadAllBytes(Path.Combine(AppContext.BaseDirectory, "Files", "Control_1.bwav"));
+    private static readonly byte[] _asset2 = File.ReadAllBytes(Path.Combine(AppContext.BaseDirectory, "Files", "Control_2.bwav"));
     private static readonly byte[] _metadata1 = File.ReadAllBytes(Path.Combine(AppContext.BaseDirectory, "Files", "Control_1.amta"));
     private static readonly byte[] _metadata2 = File.ReadAllBytes(Path.Combine(AppContext.BaseDirectory, "Files", "Control_2.amta"));
 
-    private static readonly byte[] _fileLe = File.ReadAllBytes(Path.Combine(AppContext.BaseDirectory, "Files", "Control_LE.bars"));
+    private static readonly byte[] _file = File.ReadAllBytes(Path.Combine(AppContext.BaseDirectory, "Files", "Control.bars"));
 
     private readonly AudioResource _control = new() {
         ["Test"] = new AudioResourceAsset {
             Metadata = _metadata1,
-            Asset = _asset
+            Asset = _asset1
         },
         ["Test 2"] = new AudioResourceAsset {
             Metadata = _metadata2,
-            Asset = _asset
+            Asset = _asset2
         }
     };
     
     [Fact]
-    public void SerializedLE_ShouldEqual_FileLE()
+    public void Serialized_ShouldEqual_File()
     {
-        _control.ToBinary(endianness: Endianness.Little).Should().BeEquivalentTo(_fileLe);
+        _control.ToBinary().Should().BeEquivalentTo(_file);
     }
     
     [Fact]
-    public void DeserializedLE_ShouldEqual_Control()
+    public void Deserialized_ShouldEqual_Control()
     {
-        AudioResource.FromBinary(_fileLe).Should().BeEquivalentTo(_control);
+        AudioResource.FromBinary(_file).Should().BeEquivalentTo(_control);
     }
 }
